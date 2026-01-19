4caf27820ba9d641c4967c4d1e3e75462f3da287
ps248010-tech <ps248010@g.sguc.ac.jp>
2026-01-19 21:29:25 +0900

    Implement API call to reserve endpoint
    Add fetch request to reserve API endpoint

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // Python（Colab）側のAPIのURL
 const API_BASE = "https://profit-friend-worldcat-bring.trycloudflare.com";
+const res = await fetch(API_BASE + "/api/reserve", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(data)
+});
