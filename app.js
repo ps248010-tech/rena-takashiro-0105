2994a06ca3963fe859f907c8ca742f698632afac
ps248010-tech <ps248010@g.sguc.ac.jp>
2026-01-21 21:01:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Python（Colab）側のAPIのURL
-const API_BASE = "https://sign-referenced-dialog-focused.trycloudflare.com";
+const API_BASE = "https://cities-sara-citysearch-orchestra.trycloudflare.com";
 
 // ページが読み込まれてから実行
 document.addEventListener("DOMContentLoaded", () => {
